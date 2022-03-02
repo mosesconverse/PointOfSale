384599535718fa48c17977af1414f3b67eb87be9
mosesconverse <74323649+mosesconverse@users.noreply.github.com>
2022-03-02 16:30:52 +0300

    Add files via upload

---
+++ error.php
@@ -0,0 +1,8 @@
+
+<?php 
+
+if (count($error) > 0): ?>
+		<?php foreach ($error as $error): ?>
+			<?php echo '<script>swal("","'.$error.'","error");</script>'; ?>
+		<?php endforeach ?>
+<?php endif ?>	
